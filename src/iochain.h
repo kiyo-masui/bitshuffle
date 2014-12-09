4a95f1ada3ddc49ef6bfc427cd412e2c16066172
Kiyoshi Masui <kiyo@physics.ubc.ca>
2014-12-08 17:04:14 -0800

    Wrapped openMP library calls in preprocessor directives checking for openMP.

--- src/iochain.h
+++ src/iochain.h
@@ -40,20 +40,26 @@
 
 
 #include <stdlib.h>
+#ifdef _OPENMP
 #include <omp.h>
+#endif
 
 
 #define IOC_SIZE 33
 
 
 typedef struct ioc_ptr_and_lock {
+#ifdef _OPENMP
     omp_lock_t lock;
+#endif
     void *ptr;
 } ptr_and_lock;
 
 
 typedef struct ioc_chain {
+#ifdef _OPENMP
     omp_lock_t next_lock;
+#endif
     size_t next;
     ptr_and_lock in_pl[IOC_SIZE];
     ptr_and_lock out_pl[IOC_SIZE];
@@ -61,11 +67,13 @@ typedef struct ioc_chain {
 
 
 void ioc_init(ioc_chain *C, void *in_ptr_0, void *out_ptr_0) {
+#ifdef _OPENMP
     omp_init_lock(&C->next_lock);
     for (size_t ii = 0; ii < IOC_SIZE; ii ++) {
         omp_init_lock(&(C->in_pl[ii].lock));
         omp_init_lock(&(C->out_pl[ii].lock));
     }
+#endif
     C->next = 0;
     C->in_pl[0].ptr = in_ptr_0;
     C->out_pl[0].ptr = out_ptr_0;
@@ -73,49 +81,63 @@ void ioc_init(ioc_chain *C, void *in_ptr_0, void *out_ptr_0) {
 
 
 void ioc_destroy(ioc_chain *C) {
+#ifdef _OPENMP
     omp_destroy_lock(&C->next_lock);
     for (size_t ii = 0; ii < IOC_SIZE; ii ++) {
         omp_destroy_lock(&(C->in_pl[ii].lock));
         omp_destroy_lock(&(C->out_pl[ii].lock));
     }
+#endif
 }
 
 
 void * ioc_get_in(ioc_chain *C, size_t *this_iter) {
+#ifdef _OPENMP
     omp_set_lock(&C->next_lock);
     #pragma omp flush
+#endif
     *this_iter = C->next;
     C->next ++;
+#ifdef _OPENMP
     omp_set_lock(&(C->in_pl[*this_iter % IOC_SIZE].lock));
     omp_set_lock(&(C->in_pl[(*this_iter + 1) % IOC_SIZE].lock));
     omp_set_lock(&(C->out_pl[(*this_iter + 1) % IOC_SIZE].lock));
     omp_unset_lock(&C->next_lock);
+#endif
     return C->in_pl[*this_iter % IOC_SIZE].ptr;
 }
 
 
 void ioc_set_next_in(ioc_chain *C, size_t* this_iter, void* in_ptr) {
     C->in_pl[(*this_iter + 1) % IOC_SIZE].ptr = in_ptr;
+#ifdef _OPENMP
     omp_unset_lock(&(C->in_pl[(*this_iter + 1) % IOC_SIZE].lock));
+#endif
 }
 
 
 void * ioc_get_out(ioc_chain *C, size_t *this_iter) {
+#ifdef _OPENMP
     omp_set_lock(&(C->out_pl[(*this_iter) % IOC_SIZE].lock));
     #pragma omp flush
+#endif
     void *out_ptr = C->out_pl[*this_iter % IOC_SIZE].ptr;
+#ifdef _OPENMP
     omp_unset_lock(&(C->out_pl[(*this_iter) % IOC_SIZE].lock));
+#endif
     return out_ptr;
 }
 
 
 void ioc_set_next_out(ioc_chain *C, size_t *this_iter, void* out_ptr) {
     C->out_pl[(*this_iter + 1) % IOC_SIZE].ptr = out_ptr;
+#ifdef _OPENMP
     omp_unset_lock(&(C->out_pl[(*this_iter + 1) % IOC_SIZE].lock));
     // *in_pl[this_iter]* lock released at the end of the iteration to avoid being
     // overtaken by previous threads and having *out_pl[this_iter]* corrupted.
     // Especially worried about thread 0, iteration 0.
     omp_unset_lock(&(C->in_pl[(*this_iter) % IOC_SIZE].lock));
+#endif
 }
 
 
