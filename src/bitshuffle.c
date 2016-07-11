0162637d1dc7b2b58e9ced6c16e81d3749b57f13
V. Armando Sole <sole@esrf.fr>
2016-07-11 18:41:45 +0200

    Do not use unnecessary stdint.h include.

--- src/bitshuffle.c
+++ src/bitshuffle.c
@@ -13,16 +13,6 @@
 #include "bitshuffle_core.h"
 #include "bitshuffle_internals.h"
 #include "lz4.h"
-#if defined (__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)   /* C99 */
-# include <stdint.h>
-#else
-  typedef unsigned char       uint8_t;
-  typedef unsigned short      uint16_t;
-  typedef unsigned int        uint32_t;
-  typedef signed int        int32_t;
-  typedef unsigned long long  uint64_t;
-  typedef long long           int64_t;
-#endif
 
 #include <stdio.h>
 #include <string.h>
--- src/bitshuffle.h
+++ src/bitshuffle.h
@@ -27,17 +27,6 @@
 
 #ifndef BITSHUFFLE_H
 #define BITSHUFFLE_H
-#if defined (__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)   /* C99 */
-# include <stdint.h>
-#else
-  typedef unsigned char       uint8_t;
-  typedef unsigned short      uint16_t;
-  typedef unsigned int        uint32_t;
-  typedef   signed int        int32_t;
-  typedef unsigned long long  uint64_t;
-  typedef long long           int64_t;
-#endif
-
 
 #include <stdlib.h>
 #include "bitshuffle_core.h"
