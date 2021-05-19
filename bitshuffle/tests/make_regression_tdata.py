392d23463c2f368b28517f0125377460315a3173
james-s-willis <james_willis@hotmail.co.uk>
2021-05-19 16:19:01 -0400

    Add ZSTD compressed data as another dataset to regression test file.

--- bitshuffle/tests/make_regression_tdata.py
+++ bitshuffle/tests/make_regression_tdata.py
@@ -16,30 +16,37 @@ COMP_LVL = 10 # ZSTD compression level
 FILTER_PIPELINE = [h5.H5FILTER,]
 FILTER_OPTS = [[(BLOCK_SIZE, h5.H5_COMPRESS_LZ4)], [(BLOCK_SIZE, h5.H5_COMPRESS_ZSTD, COMP_LVL)]]
 
-OUT_FILE = ["bitshuffle/tests/data/lz4_regression_%s.h5" % bitshuffle.__version__,
-            "bitshuffle/tests/data/zstd_regression_%s.h5" % bitshuffle.__version__]
+OUT_FILE = "bitshuffle/tests/data/regression_%s.h5" % bitshuffle.__version__
 
 DTYPES = ['a1', 'a2', 'a3', 'a4', 'a6', 'a8', 'a10']
 
-for i, fil_op in enumerate(FILTER_OPTS):
-    f = h5py.File(OUT_FILE[i], 'w')
-    g_comp = f.create_group("compressed")
-    g_orig = f.create_group("origional")
-    for dtype in DTYPES:
-        for rep in ['a', 'b', 'c']:
-            dset_name = "%s_%s" % (dtype, rep)
-            dtype = np.dtype(dtype)
-            n_elem = 3 * BLOCK_SIZE + random.randint(0, BLOCK_SIZE)
-            shape = (n_elem,)
-            chunks = shape
-            data = random.randint(0, 255, n_elem * dtype.itemsize)
-            data = data.astype(np.uint8).view(dtype)
-    
-            g_orig.create_dataset(dset_name, data=data)
-    
-            h5.create_dataset(g_comp, bytes(dset_name, 'utf-8'), shape, dtype, chunks=chunks,
-                    filter_pipeline=FILTER_PIPELINE, filter_flags=(h5z.FLAG_MANDATORY,), 
-                    filter_opts=fil_op)
-            g_comp[dset_name][:] = data
-    
-    f.close()
+f = h5py.File(OUT_FILE, 'w')
+g_orig = f.create_group("origional")
+g_comp_lz4 = f.create_group("compressed")
+g_comp_zstd = f.create_group("compressed_zstd")
+
+for dtype in DTYPES:
+    for rep in ['a', 'b', 'c']:
+        dset_name = "%s_%s" % (dtype, rep)
+        dtype = np.dtype(dtype)
+        n_elem = 3 * BLOCK_SIZE + random.randint(0, BLOCK_SIZE)
+        shape = (n_elem,)
+        chunks = shape
+        data = random.randint(0, 255, n_elem * dtype.itemsize)
+        data = data.astype(np.uint8).view(dtype)
+
+        g_orig.create_dataset(dset_name, data=data)
+
+        # Create LZ4 compressed data
+        h5.create_dataset(g_comp_lz4, bytes(dset_name, 'utf-8'), shape, dtype, chunks=chunks,
+                filter_pipeline=FILTER_PIPELINE, filter_flags=(h5z.FLAG_MANDATORY,), 
+                filter_opts=FILTER_OPTS[0])
+        g_comp_lz4[dset_name][:] = data
+        
+        # Create ZSTD compressed data
+        h5.create_dataset(g_comp_zstd, bytes(dset_name, 'utf-8'), shape, dtype, chunks=chunks,
+                filter_pipeline=FILTER_PIPELINE, filter_flags=(h5z.FLAG_MANDATORY,), 
+                filter_opts=FILTER_OPTS[1])
+        g_comp_zstd[dset_name][:] = data
+
+f.close()
--- bitshuffle/tests/test_regression.py
+++ bitshuffle/tests/test_regression.py
@@ -13,20 +13,21 @@ import h5py
 
 import bitshuffle
 from bitshuffle import h5
+from packaging import version
 
 
 TEST_DATA_DIR = path.dirname(bitshuffle.__file__) + "/tests/data"
 
-OUT_FILE_TEMPLATE = TEST_DATA_DIR + "/%s_regression_%s.h5"
+OUT_FILE_TEMPLATE = TEST_DATA_DIR + "/regression_%s.h5"
 
-VERSIONS = ["0.3.6",]
+VERSIONS = ["0.1.3", "0.3.6",]
 
 
 class TestAll(unittest.TestCase):
 
-    def test_lz4_regression(self):
-        for version in VERSIONS:
-            file_name = OUT_FILE_TEMPLATE % ("lz4", version)
+    def test_regression(self):
+        for rev in VERSIONS:
+            file_name = OUT_FILE_TEMPLATE % (rev)
             f = h5py.File(file_name)
             g_orig = f["origional"]
             g_comp = f["compressed"]
@@ -34,16 +35,13 @@ class TestAll(unittest.TestCase):
             for dset_name in g_comp.keys():
                 self.assertTrue(np.all(g_comp[dset_name][:]
                                        == g_orig[dset_name][:]))
-    def test_zstd_regression(self):
-        for version in VERSIONS:
-            file_name = OUT_FILE_TEMPLATE % ("zstd", version)
-            f = h5py.File(file_name)
-            g_orig = f["origional"]
-            g_comp = f["compressed"]
-
-            for dset_name in g_comp.keys():
-                self.assertTrue(np.all(g_comp[dset_name][:]
-                                       == g_orig[dset_name][:]))
+            
+            # Only run ZSTD comparison on versions >= 0.3.6
+            if version.parse(rev) >= version.parse("0.3.6"):
+                g_comp_zstd = f["compressed_zstd"]
+                for dset_name in g_comp_zstd.keys():
+                    self.assertTrue(np.all(g_comp_zstd[dset_name][:]
+                                           == g_orig[dset_name][:]))
 
 if __name__ == "__main__":
     unittest.main()
