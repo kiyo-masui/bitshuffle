a7c1ff7a0b453006a9ed873f120fa8e69b3bcf65
james-s-willis <james_willis@hotmail.co.uk>
2021-06-28 13:59:51 -0400

    Use curl instead of wget.

--- .github/workflows/install_hdf5.sh
+++ .github/workflows/install_hdf5.sh
@@ -1,4 +1,4 @@
-wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.7/src/hdf5-1.10.7.tar.gz
+curl https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.7/src/hdf5-1.10.7.tar.gz --output hdf5-1.10.7.tar.gz --silent
 tar -xvf hdf5-1.10.7.tar.gz
 cd hdf5-1.10.7
 chmod +x autogen.sh
