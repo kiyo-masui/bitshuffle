db574e2cbb532d7ccdc5220328a9a40344458ba8
james-s-willis <james_willis@hotmail.co.uk>
2021-06-28 13:53:41 -0400

    Added script to install HDF5 from source.

--- .github/workflows/install_hdf5.sh
+++ .github/workflows/install_hdf5.sh
@@ -1,6 +1,7 @@
 wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.7/src/hdf5-1.10.7.tar.gz
 tar -xvf hdf5-1.10.7.tar.gz
 cd hdf5-1.10.7
+chmod +x autogen.sh
 ./autogen.sh
 ./configure --prefix=/usr/local/hdf5
 make -j 6
