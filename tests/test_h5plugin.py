c9ab26f4ce706da985c54c7ed7989699e6c9d40c
james-s-willis <james_willis@hotmail.co.uk>
2021-08-13 13:21:25 -0400

    Use inbuilt CIBUILDWHEEL for plugin test instead of user defined CI_BUILD_WHEEL. Define ENABLE_ZSTD in CIBW_ENVIRONMENT.

--- tests/test_h5plugin.py
+++ tests/test_h5plugin.py
@@ -27,7 +27,7 @@ else:
 
 class TestFilterPlugins(unittest.TestCase):
     @pytest.mark.skipif(
-        "CI_BUILD_WHEEL" in os.environ,
+        "CIBUILDWHEEL" in os.environ,
         reason="Can't build dynamic HDF5 plugin into bitshuffle wheel.",
     )
     def test_plugins(self):
