e9dc28e9fc70a358695d53097d280aadabe399ae
Kiyoshi Masui <kmasui@mit.edu>
2024-09-27 09:45:33 -0400

    lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 import bitshuffle
 
+
 def pytest_report_header(config):
     sse2 = bitshuffle.using_SSE2()
     avx2 = bitshuffle.using_AVX2()
     avx512 = bitshuffle.using_AVX512()
     neon = bitshuffle.using_NEON()
     return f"SSE2 {sse2}; AVX2 {avx2}; AVX512 {avx512}; NEON {neon}"
-
